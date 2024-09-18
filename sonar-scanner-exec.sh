6677ea9beeef2180ebee6407a43d019a521f2f81
Alex <alexdesouzasilva94@gmail.com>
2024-09-18 17:57:05 -0300

    add-new

--- sonar-scanner-exec.sh
+++ sonar-scanner-exec.sh
@@ -3,7 +3,8 @@ SONAR_HOST_URL="http://localhost:9000"
 SONAR_TOKEN="squ_674d59260f8a4ee7bdcceddfb0126405979f3429"
 sonar-scanner \
             -Dsonar.projectKey=dev-sonar-actions \
-            -Dsonar.sources=. \
+            -Dsonar.sources=src \
             -Dsonar.host.url=$SONAR_HOST_URL \
-            -Dsonar.login=$SONAR_TOKEN
-            -Dsonar.version=0.0.4-RC
+            -Dsonar.login=$SONAR_TOKEN \
+            -Dsonar.version=0.0.5-RC \
+            -Dsonar.java.binaries=.
